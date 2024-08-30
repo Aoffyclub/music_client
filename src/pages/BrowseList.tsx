a0e68aefcff710b8a06f4cd7608a9028bf1a4f0d
Vongsakorn Srithaworn <99707911+Aoffyclub@users.noreply.github.com>
2024-08-30 23:34:26 +0700

    update

--- src/pages/BrowseList.tsx
+++ src/pages/BrowseList.tsx
@@ -2,14 +2,26 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import axios from "axios";
-import { Category } from "@/interface/Interface";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { Category,Song } from "@/interface/Interface";
 
 const BrowseList = () => {
   const { id } = useParams();
 
   const [genres, setGenres] = useState<Category>();
+  const [songs, setSongs] = useState<Song[]>([]);
   useEffect(() => {
     getGenres(id);
+    getSongByGenres(id);
   }, [id]);
 
   const getGenres = async (id: string | undefined) => {
@@ -20,7 +32,6 @@ const BrowseList = () => {
            method: "GET",
          }).then((res) => {
             setGenres(res.data.data)
-        
             
          })
 
@@ -30,6 +41,23 @@ const BrowseList = () => {
       
     }
   };
+
+    const getSongByGenres = async (id: string | undefined) => {
+      try {
+        await axios
+          .request({
+            url: `${import.meta.env.VITE_BASE_API}/api/songgenres/${id}`,
+            method: "GET",
+          })
+          .then((res) => {
+            setSongs(res.data.data);
+            console.log(res.data.data);
+          });
+      } catch (error) {
+        toast.error("Failed to get genres");
+        console.log(error);
+      }
+    };
   return (
     <div className="flex flex-col bg-[#000] p-3 h-[100%] w-[calc(100vw-250px)] text-white overflow-scroll no-scrollbar">
       <div className="flex flex-col relative gap-2 bg-gradient-to-b from-[#070707] via-[#1d1c1c]  to-[#070707] h-[1000px] w-[100%] rounded-xl overflow-hidden">
@@ -55,6 +83,37 @@ const BrowseList = () => {
             </div>
           </div>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">No.</TableHead>
+              <TableHead>Image</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Artist</TableHead>
+              <TableHead>Create</TableHead>
+              <TableHead className="text-right">Duration</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {songs.map((song, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>
+                  <img
+                    src={song.image}
+                    alt=""
+                    className="h-[50px] w-[50px] rounded-md shadow-sm"
+                  />
+                </TableCell>
+                <TableCell>{song.songName}</TableCell>
+                <TableCell>{song.artist}</TableCell>
+                <TableCell>{song.createdAt.slice(0,10)}</TableCell>
+                <TableCell className="text-right">{song.duration}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
